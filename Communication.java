84cc4ef7c2e3558df87aa1f4b605b19d7ac3108b
JagadeepSai <jagadeepsai28@gmail.com>
2019-10-01 21:08:36 +0530

    Value change incomplete

--- Communication.java
+++ Communication.java
@@ -3,6 +3,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +16,7 @@ import java.util.Map.Entry;
 public class Communication {
     private static Communication comm = null;
     static Integer numNodes = 0;
+    Integer receiveTimeout = 2000;
     Map<Integer, InetAddress> nodesIp;
     Map<Integer, Integer> sendingPort;
     Map<Integer, Integer> receivingPort;
@@ -47,7 +49,10 @@ public class Communication {
         sendingPort.put(id, sendPort);
         receivingPort.put(id, recPort);
         sendingSocket.put(id, new DatagramSocket(sendPort, ip));
-        receivingSocket.put(id, new DatagramSocket(recPort, ip));
+        
+        DatagramSocket recSocket = new DatagramSocket(recPort, ip); 
+        recSocket.setSoTimeout(receiveTimeout);
+        receivingSocket.put(id,recSocket);
     }
 
     public void removeNode(Integer id){
@@ -62,23 +67,27 @@ public class Communication {
     }
 
     public void send(String str, Integer senderId, Integer receiverId) throws IOException {
-        debug("Node sending " + senderId + " " + receiverId + " Msg : " + str);
+        // debug("Node sending " + senderId + " " + receiverId + " Msg--" + str);
         DatagramPacket DpSend = new DatagramPacket(str.getBytes(), str.length(), nodesIp.get(receiverId),receivingPort.get(receiverId));
 
         sendingSocket.get(senderId).send(DpSend);
-        debug("Node sent " + senderId + " " + receiverId + ":"+ receivingPort.get(receiverId) + " Msg : " + str );
+        // debug("Node sent " + senderId + " " + receiverId + ":"+ receivingPort.get(receiverId) + " Msg--" + str );
 
     }
 
     public String receive(Integer receiverId) throws IOException {
         String rec = null;
-        debug("Node receiving " + receiverId + ":" + receivingSocket.get(receiverId).getLocalPort());
-
+        // debug("Node receiving " + receiverId + ":" + receivingSocket.get(receiverId).getLocalPort());
+        
         byte[] receive = new byte[65535];
         DatagramPacket DpReceive = new DatagramPacket(receive, receive.length);
-        receivingSocket.get(receiverId).receive(DpReceive);
+        try {
+            receivingSocket.get(receiverId).receive(DpReceive);
+        } catch (SocketTimeoutException e) {
+            return "TIMEOUT";
+        }
         rec = new String(receive, StandardCharsets.UTF_8);
-        debug("Node received " + receiverId + " Msg : " + rec );
+        // debug("Node received " + receiverId + " Msg--" + rec );
         
         return rec;
     }
@@ -97,6 +106,11 @@ public class Communication {
         }
     }
 
+    public boolean checkMajority(int num) {
+        // System.out.println(num);
+        return (num > (numNodes + 1)/2);
+
+    }
     
     public void closeAll(){
         for (Map.Entry<Integer, Integer> pair : sendingPort.entrySet()) {
--- Paxos.java
+++ Paxos.java
@@ -1,19 +1,20 @@
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Paxos {
-    Map<Integer, Node<String> > nodeList;
+    Map<Integer, Node > nodeList;
     private static Integer numNodes = 0;
 
     Paxos(){
         nodeList = new HashMap<>();
     }
     Communication comm = Communication.getInstance(); // Object to send messages
-    Node<String> addNode(InetAddress ip, Integer sendPort, Integer recPort) throws IOException {
-        Node<String> node = new Node<>(numNodes);
+    Node addNode(InetAddress ip, Integer sendPort, Integer recPort) throws IOException {
+        Node node = new Node(numNodes);
         comm.addNode(numNodes, ip, sendPort, recPort);
         comm.numNodes++;
         numNodes++;
@@ -29,12 +30,19 @@ public class Paxos {
         // Integer p2 =  s2.getLocalPort();
         // s1.close();
         // s2.close();
-
-        Node<String> n1=paxos.addNode(localIp, 7090, 7091);
-        Node<String> n2=paxos.addNode(localIp, 7100, 7092);
-        n1.start();
-        System.out.println("temp");
-        n2.start();
+        ArrayList<Node> nodeList = new ArrayList<>();
+        int nn = 10;
+        for (int i = 0; i < nn; i++) {
+            Node n1=paxos.addNode(localIp, 7000+2*i, 7001+2*i);
+            nodeList.add(n1);
+        }
+        for (int i = 0; i < nn; i++) {
+            nodeList.get(i).start();
+        }
+        paxos.comm.send("CMDPREPARE:50", 0, 1);
+        paxos.comm.send("CMDPREPARE:51", 0, 2);
+        paxos.comm.send("CMDPREPARE:52", 0, 3);
+        paxos.comm.send("CMDPREPARE:53", 0, 4);
         // paxos.addNode(localIp, 7080);
         
     }
